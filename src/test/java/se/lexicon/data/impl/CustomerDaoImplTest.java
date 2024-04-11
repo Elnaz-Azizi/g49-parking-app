42cf7f1433d9894d6fb9624c552d365e3a33f02f
Elnaz <azizi.elnaz66@gmail.com>
2024-04-11 15:03:38 +0200

    Implemented some parts of ParkingSpotDao.

--- src/test/java/se/lexicon/data/impl/CustomerDaoImplTest.java
+++ src/test/java/se/lexicon/data/impl/CustomerDaoImplTest.java
@@ -1,10 +1,12 @@
 package se.lexicon.data.impl;
 
-import org.junit.jupiter.api.Assertions;
+import static org.junit.jupiter.api.Assertions.*;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import se.lexicon.model.Customer;
 
+import java.util.List;
 import java.util.Optional;
 
 public class CustomerDaoImplTest {
@@ -19,58 +21,72 @@ public class CustomerDaoImplTest {
     @Test
     public void testCreateCustomer() {
 
-        Customer customer1 = new Customer("Elnaz","123456");
+        Customer customer1 = new Customer("Elnaz", "123456");
         Customer actualCustomer = testObject.create(customer1); // 1001
 
-        Customer expectedCustomer = new Customer(1001, "Elnaz","123456");
+        Customer expectedCustomer = new Customer(1001, "Elnaz", "123456");
 
 
-        Assertions.assertEquals(expectedCustomer.getId(), actualCustomer.getId());
-        Assertions.assertEquals(expectedCustomer.getName(), actualCustomer.getName());
+        assertEquals(expectedCustomer.getId(), actualCustomer.getId());
+        assertEquals(expectedCustomer.getName(), actualCustomer.getName());
 
     }
-
-
-
 
     @Test
     public void testFindById() {
 
-        Customer customer1 = new Customer("Elnaz","123456");
+        Customer customer1 = new Customer("Elnaz", "123456");
         Customer createdCustomer = testObject.create(customer1);
         Optional<Customer> foundCustomer = testObject.find(1001);
 
-        Assertions.assertTrue(foundCustomer.isPresent());
+        assertTrue(foundCustomer.isPresent());
 
     }
 
     @Test
     public void testNonExistentCustomer() {
-        //todo: Implement JUnit test
+        Optional<Customer> foundCustomer = testObject.find(989);
+        assertFalse(foundCustomer.isPresent());
 
     }
 
     @Test
     public void testRemoveCustomer() {
-        //todo: Implement JUnit test
+        Customer customer1 = new Customer("Elnaz", "123456");
+        testObject.create(customer1);
+        boolean removed = testObject.remove(customer1.getId());
+        assertTrue(removed);
+        assertFalse(testObject.find(customer1.getId()).isPresent());
 
     }
 
     @Test
     public void testRemoveNonExistentCustomer() {
-        //todo: Implement JUnit test
+        boolean removed = testObject.remove(900);
+        assertFalse(removed);
 
     }
 
     @Test
     public void testFindAllCustomers() {
-        //todo: Implement JUnit test
+        Customer customer1= new Customer("Elnaz", "123456");
+        Customer customer2= new Customer("Nickan", "246810");
+        Customer customer3= new Customer("Marry", "357911");
+        testObject.create(customer1);
+        testObject.create(customer2);
+        testObject.create(customer3);
+        List<Customer> customers= testObject.findAll();
+        assertEquals(3, customers.size());
+        assertTrue(customers.contains(customer1));
+        assertTrue(customers.contains(customer2));
+        assertTrue(customers.contains(customer3));
 
     }
 
     @Test
     public void testFindAllCustomersEmptyList() {
-        //todo: Implement JUnit test
+        List<Customer> customers = testObject.findAll();
+        assertTrue(customers.isEmpty());
 
     }
 
--- src/test/java/se/lexicon/data/impl/ParkingSpotDaoImplTest.java
+++ src/test/java/se/lexicon/data/impl/ParkingSpotDaoImplTest.java
@@ -1,6 +1,33 @@
 package se.lexicon.data.impl;
 
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import se.lexicon.model.ParkingSpot;
+
 public class ParkingSpotDaoImplTest {
-    //todo: Implement required JUnit tests
+    private ParkingSpotDaoImpl parkingSpotDao;
+
+    @BeforeEach
+    public void setUp() {
+        parkingSpotDao = new ParkingSpotDaoImpl();
+    }
+
+
+    @Test
+
+    public void testCreat() {
+
+        ParkingSpot parkingSpot = new ParkingSpot(1, 47001);
+        ParkingSpot actualParkingSpot = parkingSpotDao.create(parkingSpot);
+        ParkingSpot expectedParkingSpot = parkingSpot;
+        assertTrue(parkingSpotDao.find(1).isPresent());
+        assertEquals(expectedParkingSpot.getSpotNumber(), actualParkingSpot.getSpotNumber());
+    }
+
+
 
 }
+
+
